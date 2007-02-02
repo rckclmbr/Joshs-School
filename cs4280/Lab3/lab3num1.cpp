2a169dff17d95774d7a9563149beecf51a11b914
arnie <arnie@67c49fff-0526-0410-8e7c-b4c1397322bd>
2007-02-02 01:54:17 +0000

    Essentially finished lab 3 part 1

--- cs4280/Lab3/lab3num1.cpp
+++ cs4280/Lab3/lab3num1.cpp
@@ -1,30 +1,54 @@
-#include "header.h"
-#include "canvas.h"
+#include "includes/header.h"
+#include "includes/canvas.h"
 
-Canvas cvs(640,480,"A turtle! OMG!"); //global canvas object
+Canvas cvs(640,240,"A turtle!"); //global canvas object
  
 void display(void)
 {
     cvs.clearScreen();//clear screen
-    cvs.setWindow(-10.0,10.0,-10.0,10.0);
-    cvs.setViewport(10,640,10,460);
+    cvs.setWindow(-50.0,20.0,0,35.0);
+    cvs.setViewport(10,640,10,240);
     
-    cvs.forward(10,1);
-    cvs.turn(90);
-    cvs.forward(10,1);
-    cvs.turn(90);
-    cvs.forward(10,1);
-    cvs.turn(90);
-    cvs.forward(10,1);
-    /*
-    cvs.moveTo(0,-10.0);//draw a line
-    cvs.lineTo(0,10.0);
-    cvs.moveTo(-10.0, 0);
-    cvs.lineTo(10.0, 0);
-    RealRect box(-2.0,8.0,-1.0,9.0);//construct a box
-    box.draw();//draw the box
-    IntRect box2(-8, -3, -9, 12);
-    box2.draw();*/
+	// Move for part 1
+	cvs.moveTo(-49,2);
+	cvs.turnTo(0);
+
+	// Draw part 1
+	for(int x = 20; x > 1; x -= 4) {
+		for(int y = 0; y < 4; y++) {
+			cvs.forward(x,1);
+			cvs.turn(90);
+		}
+	}
+	
+	// Move for part 2
+	cvs.moveTo(-25,2);
+	cvs.turnTo(0);
+	
+	// Draw part 2
+	for(int x = 20; x > 1; x --) {
+		cvs.forward(x,1);
+		cvs.turn(90);
+	}
+
+	// Move for part 3
+	cvs.moveTo(0,2);
+	cvs.turnTo(0);
+
+	// Draw part 3 WITHOUT lifting the pen
+	cvs.forward(20,1);
+	cvs.turn(120);
+	cvs.forward(20,1);
+	cvs.turn(120);
+	cvs.forward(10,1);
+	cvs.turn(120);
+	cvs.forward(10,1);
+	cvs.turn(-120);
+	cvs.forward(10,1);
+	cvs.turn(-120);
+	cvs.forward(10,1);
+	cvs.turn(120);
+	cvs.forward(10,1);
 }
 
 
@@ -38,4 +62,3 @@ int main(int argc, char* argv[])
     
     return 0;
 }
-
