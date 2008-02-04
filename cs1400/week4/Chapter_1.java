2c33504d7155e023d3c7a1688fb8c8c7a8414138
arnie <arnie@67c49fff-0526-0410-8e7c-b4c1397322bd>
2008-02-04 03:20:15 +0000

    Adding cs1400 week 4 program (pretty tough, eh?)

---
+++ cs1400/week4/Chapter_1.java
@@ -0,0 +1,15 @@
+public class Chapter_1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("   -------   ");
+		System.out.println("  /       \\ ");
+		System.out.println(" /         \\");
+		System.out.println(" -\"-'-\"-'-\"- ");
+		System.out.println(" \\         / ");
+		System.out.println("  \\       /  ");
+		System.out.println("   -------   ");
+
+	}
+
+}
