13d3f69f2c7ce266cc7b2910eaad7b30061c989c
arnie <arnie@67c49fff-0526-0410-8e7c-b4c1397322bd>
2008-02-19 05:58:23 +0000

    Adding the removal script

---
+++ cs3550/HotelExtension/ddl/rev1_drop.sql
@@ -0,0 +1,35 @@
+USE Braegger_HotelEvents
+
+ALTER TABLE Booking 
+DROP CONSTRAINT FK_BookingMustHavePerformer
+
+ALTER TABLE Booking
+DROP CONSTRAINT FK_BookingCanHaveStage
+
+ALTER TABLE Booking
+DROP CONSTRAINT FK_BookingMustHaveEvent
+
+ALTER TABLE StaffClock
+DROP CONSTRAINT FK_StaffClockMustHaveEvent
+
+ALTER TABLE StaffClock
+DROP CONSTRAINT FK_StaffClockMustHaveStaff
+
+ALTER TABLE Ticket
+DROP CONSTRAINT FK_TicketMustHaveEvent
+
+ALTER TABLE Ticket
+DROP CONSTRAINT FK_TicketMustHaveViewer
+
+ALTER TABLE EventStaff
+DROP CONSTRAINT FK_EventStaffMustHaveStaffType
+
+DROP TABLE Booking
+DROP TABLE [Event]
+DROP TABLE EventStaff
+DROP TABLE Performer
+DROP TABLE StaffClock
+DROP TABLE StaffType
+DROP TABLE Stage
+DROP TABLE Ticket
+DROP TABLE Viewer
