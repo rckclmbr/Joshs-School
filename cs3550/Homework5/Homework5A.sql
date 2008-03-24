2042a97b512e387534d016eb3e9549e27681145e
arnie <arnie@67c49fff-0526-0410-8e7c-b4c1397322bd>
2008-03-24 03:30:42 +0000

    Adding homework 5

---
+++ cs3550/Homework5/Homework5A.sql
@@ -0,0 +1,293 @@
+use Braegger_Hotel;
+
+DROP PROC sp_InsertGuest
+DROP PROC sp_InsertRooms
+DROP PROC sp_InsertReservationDetail
+DROP PROC sp_UpdatePrices
+DROP PROC sp_UpdateResDetail
+DROP PROC sp_InsertReservation
+
+-- 1.  Write a stored procedure named sp_InsertGuest that can be used to insert a row of data into the GuestTable. 
+PRINT '1. Creating sp_InsertGuest stored procedure'
+
+GO
+CREATE PROC sp_InsertGuest
+@GuestFirst varchar(20),
+@GuestLast varchar(20),
+@GuestAddress1 varchar(30),
+@GuestAddress2 varchar(10) = NULL,
+@GuestCity varchar(20),
+@GuestState char(2),
+@GuestPostalCode char(10),
+@GuestCountry varchar(20),
+@GuestPhone varchar(20),
+@GuestEmail varchar(30) = NULL,
+@GuestComments varchar(200) = NULL
+
+AS
+
+INSERT INTO Guest (GuestFirst, GuestLast, GuestAddress1, GuestAddress2, GuestCity, GuestState, GuestPostalCode, GuestCountry,
+GuestPhone, GuestEmail, GuestComments) VALUES
+(@GuestFirst, @GuestLast, @GuestAddress1, @GuestAddress2, @GuestCity, @GuestState, @GuestPostalCode, @GuestCountry,
+@GuestPhone, @GuestEmail, @GuestComments)
+
+GO
+
+--    1A.  Demonstrate this procedure by adding yourself as a new guest, using sp_InsertGuest.
+PRINT '1A.  Inserting myself as a new guest using sp_InsertGuest'
+EXEC sp_InsertGuest
+@GuestFirst = 'Josh',
+@GuestLast = 'Braegger',
+@GuestAddress1 = '123 Hickory Street',
+@GuestCity = 'Bountiful',
+@GuestState = 'UT',
+@GuestPostalCode = '84010',
+@GuestCountry = 'USA',
+@GuestPhone = '801-555-5555'
+
+GO
+--    1B.  Select * From Guests to show the results.
+PRINT 'Showing results: '
+SELECT * FROM Guest
+
+--2.  Write a stored procedure named sp_InsertRooms that can be used to insert a row of data into the HotelRoomTypes AND 
+--    Room Tables. 
+PRINT '2.  Creating sp_InsertRooms'
+GO
+
+CREATE PROC sp_InsertRooms
+@HotelID smallint,
+@RoomType varchar(20),
+@RoomTypeRackRate smallmoney,
+@RoomTypeDescription varchar(200),
+@RoomNumber varchar(5),
+@RoomDescription varchar(200),
+@RoomSmoking bit,
+@RoomBedConfiguration varchar(30)
+
+AS
+
+DECLARE @RoomTypeID smallint
+
+IF NOT EXISTS (
+	SELECT HotelRoomTypeID
+	FROM HotelRoomType
+	WHERE HotelID = @HotelID
+		AND RoomType = @RoomType
+)
+BEGIN
+	INSERT INTO HotelRoomType (HotelID, RoomType, RoomTypeRackRate, RoomTypeDescription) VALUES
+	( @HotelID, @RoomType, @RoomTypeRackRate, @RoomTypeDescription )
+
+	SELECT @RoomTypeID = @@IDENTITY
+END
+
+ELSE
+	SELECT @RoomTypeID = (SELECT HotelRoomTypeID FROM HotelRoomType WHERE HotelID = @HotelID AND RoomType = @RoomType)
+
+INSERT INTO Room (HotelRoomTypeID, RoomNumber, RoomDescription, RoomSmoking, RoomBedConfiguration, HotelID) VALUES 
+(@RoomTypeID, @RoomNumber, @RoomDescription, @RoomSmoking, @RoomBedConfiguration, @HotelID)
+
+GO
+
+--    2A.  Demonstrate:  The Weber Towers and Suites is expanding.  Using sp_InsertRooms, Insert a New Room Type called 
+--         'Suite' and add Room 351, a Suite, with a rate of $199 / night and room description:  'A new suite overlooking 
+--         the Great Salt Lake'.
+
+EXEC sp_insertRooms
+@HotelID = 2300, -- Weber Towers and Sweets
+@RoomType = 'Suite',
+@RoomTypeRackRate = 199.00,
+@RoomTypeDescription = 'A nice suite',
+@RoomNumber = 351,
+@RoomDescription = 'A new suite overlooking the Great Salt Lake',
+@RoomSmoking = 0,
+@RoomBedConfiguration = 'Double'
+
+GO 
+
+--    2B.  Select * From Rooms to show the results.
+PRINT '2B. Showing results'
+SELECT * FROM Room
+
+--3.  Write a stored procedure named sp_UpdatePrices that can be used to update prices on the HotelRoomTypes Table.  
+--    The procedure will require a Hotel Number (ID) and percentage increase (or decrease) parameter to be passed.  
+--    It will set the new rate of the room - rounding down to the nearest dollar for price decreases, or rounding up to 
+--    the nearest dollar for price increases.
+GO
+PRINT '3. Creating sp_UpdatePrices'
+GO
+CREATE PROC sp_UpdatePrices
+@HotelID smallint,
+@PercentInc decimal
+
+AS
+
+	UPDATE HotelRoomType
+	SET 
+		RoomTypeRackRate = CASE 
+			WHEN @PercentInc < 0 -- Round down for price decreases  
+				THEN FLOOR(@PercentInc / 100 * RoomTypeRackRate)
+			ELSE -- Round up for price increases
+				CEILING(@PercentInc / 100 * RoomTypeRackRate)
+			END
+	WHERE HotelID = @HotelID
+
+GO 
+
+--    3A.  Demonstrate:  It's springtime in Park City, and Rich's B&B wants to decrease rates 6.5% (Rounding down to the 
+--         nearest dollar) for all their rooms.   Call sp_UpdatePrices to make this happen.  
+PRINT '3A. Demonstrating sp_UpdatePrices'
+GO
+EXEC sp_UpdatePrices
+@HotelID = 2100,    -- Rich's B&B
+@PercentInc = -6.5 -- Decreasing the price, so negative
+
+--    3B.  Select * From HotelRoomType to show the results.
+PRINT '3B. Showing results: '
+SELECT * FROM HotelRoomType
+
+--    3C.  Demonstrate:  Calling the exact same sp_UpdatePrices (only supplying a different Hotel # and %), increase the 
+--         price of all Rooms at Weber Towers and Suites 5% (rounding up to the nearest dollar). 
+EXEC sp_UpdatePrices
+@HotelID = 2300, -- Weber Towers and Suites
+@PercentInc = 5
+
+--    3D.  Select * From HotelRoomType to show the results.
+PRINT '3D. Showing results: '
+SELECT * FROM HotelRoomType
+
+--4.  Write a stored procedure named  sp_UpdateResDetail (for the ReservationsDetail Table in your DB)  that can be used 
+--    to update a reservation (and any combination of check-in date, number of nights, and/or status).  If any of the 3 
+--    values are not supplied when the procedure is executed, then then these parameters should retain their original 
+--    value.  This stored procedure should include appropriate error handling to give the user a friendly message.  For 
+--    example:  Show a friendly message like "Sorry, but 'A' is not a valid value for the number of nights.  Record not 
+--    updated."  You only need to do one error handling routine - you don't need to account for every possible error.
+PRINT '3A. Creating sp_UpdateResDetail'
+GO
+
+CREATE PROC sp_UpdateResDetail
+@ReservationDetailID smallint,
+@CheckinDate smalldatetime = NULL,
+@NumberOfNights int = NULL,
+@Status char(1) = NULL
+
+AS
+
+-- Exception check: They have to enter a valid ReservationDetailID
+IF NOT EXISTS (
+	SELECT *
+	FROM ReservationDetail
+	WHERE ReservationDetailID = @ReservationDetailID
+)
+	RAISERROR (
+		76224, -- Error Number
+		15,    -- Severity
+		1,     -- State
+		@ReservationDetailID -- Parameters
+	)
+
+
+IF @CheckinDate IS NOT NULL
+	UPDATE ReservationDetail 
+	SET CheckinDate = @CheckinDate 
+	WHERE ReservationDetailID = @ReservationDetailID
+IF @NumberOfNights IS NOT NULL
+	UPDATE ReservationDetail 
+	SET Nights = @NumberOfNights 
+	WHERE ReservationDetailID = @ReservationDetailID
+IF @Status IS NOT NULL
+	UPDATE ReservationDetail
+	SET Status = @Status
+	WHERE ReservationDetailID = @ReservationDetailID
+
+GO
+--    4A.  Demonstrate by providing any combination of nights, status, and/or check-in date changes.
+PRINT '4A. Demonstrating sp_UpdateResDetail'
+GO
+EXEC sp_UpdateResDetail
+@ReservationDetailID = 1,
+@NumberOfNights = 2
+
+GO
+
+--    4B.  Do it one more time with different combinations.
+PRINT '4B. Demonstrating sp_UpdateResDetail (again...)'
+GO
+EXEC sp_UpdateResDetail
+@ReservationDetailID = 9,
+@CheckinDate = '6/9/2008',
+@Status = 'A'
+
+--    4C.  Do it a third time so it triggers your error.
+PRINT '4C. Demonstrating sp_UpdateResDetail so it triggers my error'
+
+--TODO: Do this
+
+--5.  Write a stored procedure named sp_InsertReservation that can be used to insert reservation data into the 
+--    Reservations Table AND ReservationDetails Table.  You will need to retrieve the value of @@IDENTITY from the 
+--    reservationID, then populate the First Reservation Detail using that ID.  Note: Break this down into 2 stored 
+--    procedures (1 for Reservation and one for reservation details).  By keeping a separate reservation detail sproc, 
+--    we would be able to run the detail part as many times as needed to accommodate multiple reservation details.
+PRINT '5.  Creating sp_InsertReservation'
+GO
+CREATE PROC sp_InsertReservation
+@CreditCardID smallint,
+@ReservationDate smalldatetime,
+@ReservationStatus char(1),
+@ReservationComments varchar(200),
+-- For reservation insert
+@RoomID smallint,
+@QuotedRate smallmoney,
+@CheckinDate smalldatetime,
+@Nights tinyint,
+@DiscountID smallint
+
+AS
+
+INSERT INTO Reservation (CreditCardID, ReservationDate, ReservationStatus, ReservationComments) VALUES
+(@CreditCardID, @ReservationDate, @ReservationStatus, @ReservationComments)
+
+EXEC sp_InsertReservationDetail
+@RoomID = @RoomID,
+@ReservationID = @@IDENTITY,
+@QuotedRate = @QuotedRate,
+@CheckinDate = @CheckinDate,
+@Nights = @Nights,
+@DiscountID = @DiscountID
+
+GO
+PRINT '5. (cont) Creating sp_InsertReservationDetail'
+GO
+CREATE PROC sp_InsertReservationDetail
+@RoomID smallint,
+@ReservationID smallint,
+@QuotedRate smallmoney,
+@CheckinDate smallint,
+@Nights tinyint,
+@Comments varchar(200) = NULL,
+@DiscountID smallint,
+@Status char(1) = NULL
+
+AS
+
+INSERT INTO ReservationDetail (RoomID, ReservationID, QuotedRate, CheckinDate, Nights, Status, Comments, DiscountID)
+VALUES
+(@RoomID, @ReservationID, @QuotedRate, @CheckinDate, @Nights, @Status, @Comments, @DiscountID)
+
+GO
+
+--    5A.  Demonstrate:  Using sp_InsertReservations, make a single reservation, with 3 details*, for yourself at the 
+--         Weber Towers and Suites on April 21st for 2 nights (the suite), a reservation for the Rich's B&B on April 28th 
+--         for 2 nights, (double room), non-smoking, and a reservation for the Rich's B&B on May 28th for 2 nights, 
+--         (double room), non-smoking.  
+--         *You'll run the second part of the stored procedure (in #5) three times, with different data.
+PRINT '5A. Creating reservation using sp_InsertReservations (TODO)'
+-- TODO: ?
+
+--    5B.  Select * From Reservations and ReservationDetails to show the results. 
+
+PRINT '5B.  Showing results'
+
+SELECT * FROM Reservation
+SELECT * FROM ReservationDetail
---
+++ cs3550/Homework5/Homework5B.sql
@@ -0,0 +1,210 @@
+use Braegger_Hotel
+
+DROP FUNCTION dbo.fn_GetTotalRoomCharge
+DROP FUNCTION dbo.fn_GetTotalTax
+DROP FUNCTION dbo.fn_ProduceBill
+DROP FUNCTION dbo.fn_AnticipatedRevenue
+
+
+-- In this project, you will start to develop data components for the Hotel On-time Booking System (HOBS). 
+-- The purpose of the system is to help in planning vacancies, reservations, and revenue for SQL Hotels, Inc. 
+-- Use your freshly created (since 5-A) database on TITAN: YOURLASTNAME_HOTEL to run your script against.
+
+-- 1A. Write a user-defined function named dbo.ProduceBill that accepts only the input parameter 'ReservationDetailID' 
+--     to generate a printed bill (nothing fancy - use SELECT statements) displaying the guest's information (Name, 
+--     address, etc), reservation details (Hotel Name, Room #, Check-in Date, Check-Out Date, Total Room Charge, 
+--     Total Tax). Note: This may require more than one user defined function...and/or the use of a cursor.
+PRINT '1A. Creating dbo.ProduceBill'
+GO
+
+CREATE FUNCTION dbo.fn_GetTotalTax (@ReservationDetailID smallint)
+RETURNS smallmoney
+AS
+BEGIN
+	RETURN (
+		SELECT 
+			dbo.fn_GetTotalRoomCharge( rd.ReservationDetailID ) * (t.RoomTaxRate / 100) AS TotalTax
+		FROM ReservationDetail rd
+			JOIN Discount d ON rd.DiscountID = d.DiscountID
+			JOIN Room r ON rd.RoomID = r.RoomID
+			JOIN Hotel h ON r.HotelID = h.HotelID
+			JOIN TaxRate t ON h.TaxLocationID = t.TaxLocationID
+		WHERE rd.ReservationDetailID = @ReservationDetailID
+	)
+END
+
+GO
+
+CREATE FUNCTION dbo.fn_GetTotalRoomCharge(@ReservationDetailID smallint)
+RETURNS smallmoney
+AS
+BEGIN
+	RETURN (
+		SELECT 
+			(rd.QuotedRate * Nights) * ((100 - d.DiscountPercent) / 100) - d.DiscountAmount 
+				AS TotalRoomCharge
+		FROM ReservationDetail rd
+			JOIN Discount d ON rd.DiscountID = d.DiscountID
+			JOIN Room r ON rd.RoomID = r.RoomID
+			JOIN Hotel h ON r.HotelID = h.HotelID
+			JOIN TaxRate t ON h.TaxLocationID = t.TaxLocationID
+		WHERE rd.ReservationDetailID = @ReservationDetailID
+	)
+END
+
+GO
+
+CREATE FUNCTION dbo.fn_ProduceBill (@ReservationDetailID smallint)
+RETURNS @Report table (
+	FirstName varchar(30),
+	LastName varchar(30),
+	Address varchar(64),
+	City varchar(32),
+	State char(2),
+	PostalCode varchar(32),
+	Country varchar(32),
+	HotelName varchar(32),
+	RoomNumber varchar(5),
+	CheckinDate smalldatetime,
+	CheckoutDate smalldatetime,
+	TotalRoomCharge smallmoney,
+	TotalTax smallmoney
+)
+AS
+BEGIN
+	INSERT INTO @Report
+
+		SELECT 
+			GuestFirst AS FirstName,
+			GuestLast AS LastName,
+			GuestAddress1 AS Address,
+			GuestCity AS City,
+			GuestState AS State,
+			GuestPostalCode AS PostalCode,
+			GuestCountry AS Country,
+			h.HotelName AS HotelName,
+			r.RoomNumber AS RoomNumber,
+			rd.CheckinDate AS CheckinDate,
+			rd.CheckinDate + Nights AS CheckoutDate,
+			-- Calculate room charge
+			dbo.fn_GetTotalRoomCharge( rd.ReservationDetailID ) AS TotalRoomCharge,
+			dbo.fn_GetTotalTax( rd.ReservationDetailID ) AS TotalTax
+		FROM ReservationDetail rd
+			JOIN Reservation res ON rd.ReservationID = res.ReservationID
+			JOIN CreditCard cc ON res.CreditCardID = cc.CreditCardID
+			JOIN Guest g ON cc.GuestID = g.GuestID
+			JOIN Room r ON rd.RoomID = r.RoomId
+			JOIN Hotel h ON r.HotelID = h.HotelID
+		WHERE ReservationDetailID = @ReservationDetailID
+	
+	RETURN
+END
+GO
+-- 1B. Demonstrate this function with a query that contains a master reservation number (pick one starting with 5000). 
+--     Then, starting with ReservationDetailID 1, Loop through calling dbo.ProduceBill and displaying (if count > 0) 
+--     the bill for each Reservation Detail associated with that reservation number.
+PRINT '1B.  Demonstrating the dbo.ProduceBill function'
+
+DECLARE @ResDetailStopPoint smallint
+DECLARE @ReservationID smallint
+DECLARE @ReservationDetailIDPostion smallint
+
+SET @ReservationID = 5010 --(The Reservation # you want the details for)
+SET @ReservationDetailIDPostion = 1 --(Starting Res Detail # in the table)
+ 
+
+-- Modified this from the sample so that only one row is returned (makes more sense to me)
+SELECT TOP 1 @ResDetailStopPoint = ReservationDetailID
+FROM ReservationDetail
+WHERE ReservationID = @ReservationID
+ORDER BY ReservationDetailID DESC
+
+--Here is the loop
+WHILE (@ReservationDetailIDPostion <= @ResDetailStopPoint)
+BEGIN
+
+	--As long as there's at least 1 row returned....
+	IF (
+		SELECT COUNT(*) 
+		FROM ReservationDetail
+		WHERE ReservationDetailID = @ReservationDetailIDPostion 
+			AND ReservationID = @ReservationID
+	) > 0
+		SELECT *	
+		FROM dbo.fn_ProduceBill( @ReservationDetailIDPostion )
+
+		SET @ReservationDetailIDPostion = @ReservationDetailIDPostion +1
+END
+
+GO
+
+-- 2A. Write a user-defined function named dbo.AnticipatedRevenue to calculate the total actual (Status="B") plus 
+--     anticipated (Status = "A") revenue and taxes from the reservations details table, given a date range input 
+--     parameter (Use Error Handling if first date > ending date)*. The function returns a table containing the total 
+--     revenue and tax for each hotel number, for a given date range. Remember, Revenue is based off of Checkout, 
+--     not Check-in date. *Note: Functions do not allow RAISERROR, like stored procedures, so at a minimum, you'll 
+--     have to return the error through the function (in this case, as a table entry). HINT: you can query the 
+--     returned table to find out if the error was written to it, delete the error entry, and notify the user. See 
+--     Clues
+PRINT '2A. Creating dbo.AnticipatedRevenue'
+GO
+CREATE FUNCTION dbo.fn_AnticipatedRevenue (@1stDate smalldatetime, @2ndDate smalldatetime)
+
+RETURNS @Report Table (
+	Revenue smallmoney,
+	Taxes smallmoney,
+	Error varchar(64)
+)
+
+AS
+
+BEGIN
+	DECLARE @ERROR varchar(32)
+
+	IF (@1stDate > @2ndDate)
+	BEGIN
+		SET @ERROR = 'The 2nd Date is less than the first date'
+		INSERT INTO @Report VALUES (NULL, NULL, @ERROR)
+	END
+
+	ELSE
+
+	BEGIN
+		DECLARE @Revenue numeric
+		DECLARE @Taxes  numeric
+
+		SELECT
+			@Revenue = TotalRoomCharge,
+			@Taxes = TotalRoomTax
+		-- Break this out, for readability sake
+		FROM (
+			SELECT
+				SUM( dbo.fn_GetTotalRoomCharge( rd.ReservationDetailID ) ) AS TotalRoomCharge,
+				SUM( dbo.fn_GetTotalTax( rd.ReservationDetailID ) ) AS TotalRoomTax
+			FROM ReservationDetail rd
+				JOIN Room r ON rd.RoomID = r.RoomID
+				JOIN Hotel h ON r.HotelID = h.HotelID
+					JOIN TaxRate t ON h.TaxLocationID = t.TaxLocationID
+			WHERE CheckinDate + Nights BETWEEN @1stDate AND @2ndDate
+				AND rd.Status IN ('A','B')
+		) a
+		-- the real data with @Error = NULL
+		INSERT INTO @Report VALUES (
+			@Revenue, @Taxes, NULL
+		)
+	END
+
+	RETURN
+END
+
+GO
+
+-- 2B. After defining this function, pass in the dates 31 Mar - 1 Mar (yes that's right, so we can test the error),
+PRINT '2B. Testing error with invalid dates'
+
+SELECT * FROM dbo.fn_AnticipatedRevenue( '03-31-2008', '03-01-2008' )
+
+-- 2C. Do it again with the correct date sequence of 1 Mar - 31 Mar to test it out.
+PRINT '2C. Testing dates with correct sequence'
+
+SELECT * FROM dbo.fn_AnticipatedRevenue( '03-01-2008', '03-31-2008' )
